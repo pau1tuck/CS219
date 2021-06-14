42bb94edb958c8dd71cba21fe6a3dd5d855eb23f
Paul Tuck <pdrt888@gmail.com>
2021-06-15 05:32:38 +0800

    first commit

---
+++ src/rust-datatypes.rs
@@ -0,0 +1,29 @@
+
+// Declare an implicit integer
+let mut implicit = 42;
+
+// Declare an explicit integer
+let mut explicit: u8 = 255;
+
+// Declare an unsigned explicit integer
+let mut unsignedInt: u8 = 42;
+
+// Declare a signed explicit negative integer
+let mut signedInteger: i8 = -10;
+
+let mut float: f32 = 1.62; // default f64
+
+let mut boolean: bool = true;
+
+// Immutable string literal
+
+let str1 = " Hello, World! "; // &str type
+
+let str2 = str1.trim(); // owned String type
+
+let str3 = &str2; // borrow operator
+
+// Contiguous character array
+
+
+let str4 = i.to_string(); // String type (new memory allocation)
