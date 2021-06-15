a6e3444118a71dcb5ff119e4515c5118b10da43b
Paul Tuck <pau1tuck@outlook.com>
2021-06-16 05:11:21 +0800

    "2021-06-16T05:11CST"

--- Rust/strings/src/main.rs
+++ Rust/strings/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    
+
+    let mut string1 = String::new();
+    let string2 = String::from("Hello");
+    let string3 = "World".to_string();
+    println!("{}, {}!", string2, string3);
+
+    string1.push_str("Rust");
+    string1.push('🦀');
+    println!("{}, {}!", string2, string1);
 }
