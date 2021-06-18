e867625f40ba4d2ed92543180d2b2bc5875cc0b7
Paul Tuck <pau1tuck@outlook.com>
2021-06-18 08:41:31 +0800

    "2021-06-18T08:41CST"

--- Rust/projects/moon_converter/src/main.rs
+++ Rust/projects/moon_converter/src/main.rs
@@ -1,13 +1,17 @@
 use std::io;
 
 fn main() {
+    println!("Enter your weight (kg):");
     let mut input = String::new();
-    io::stdin().read_line(&mut input);
-    let moon_weight = calculate_weight_on_moon(100);
-    println!("Weight on the Moon: {} kg", calculate_weight_on_moon(moon_weight));
+    
+    io::stdin().read_line(&mut input).unwrap();
+    let earth_weight: f32 = input.trim().parse().unwrap();
+
+    let moon_weight = calculate_weight_on_moon(earth_weight);
+    println!("Your weight on the Moon is {:.2} kg.", moon_weight);
 }
 
 fn calculate_weight_on_moon(weight: f32) -> f32 {
+    // Weight on Earth / 9.81 m/s^2) * 1.622 m/s^2
     (weight / 9.81) * 1.622
 }
-
