2db3cf2366c07eba90485be882dad6d914018d51
Paul Tuck <pdrt888@gmail.com>
2021-06-21 16:09:44 +0800

    "2021-06-21T16:09CST"

--- Rust/projects/minigrep/src/main.rs
+++ Rust/projects/minigrep/src/main.rs
@@ -3,19 +3,30 @@ use std::fs;
 
 fn main() {
 
-    // The function std::env::args returns an iterator of the command line arguments that were passed to minigrep.
-    // Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection, such as a vector, containing all the elements the iterator produces:
+    // The function std::env::args returns an iterator of the command line arguments that were passed to minigrep:
     let args: Vec<String> = env::args().collect();
-
-    let query = &args[1]; // The program’s name takes up the first value in the vector at args[0], so we start at [1].
-    let filename = &args[2];
-
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    // Iterators produce a series of values, and we can call the collect method on an iterator to turn it into a collection, such as a vector, containing all the elements the iterator produces.
+    
+    
+    let config = parse_config(&args); // Destructures the variables returned by parse_config.
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
     // fs::read_to_string takes the filename, opens that file, and returns a Result<String> of the file’s contents:
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file.");
+    let contents = fs::read_to_string(config.filename)
+        .expect("Error reading the file.");
 
     println!("With text:\n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone(); // The program’s name takes up the first value in the vector at args[0], so we start at [1].
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
