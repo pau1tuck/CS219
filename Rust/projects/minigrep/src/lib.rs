956a3942daad8a444291e05e7eab7e7c97c2286b
Paul Tuck <pdrt888@gmail.com>
2021-06-29 00:13:43 +0800

    "2021-06-29T00:13CST"

--- Rust/projects/minigrep/src/lib.rs
+++ Rust/projects/minigrep/src/lib.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::fs;
 
-// A struct to name the related purpose of query and filename and to be able to return the values’ names as struct field names from the Config::new().
+// A struct to name the related purpose of query and filename and to be able to return the values’ names as struct field names from the Config::new():
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -19,19 +19,47 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{ // This function previously returned the unit type
-    // Box<dyn Error> means the function will return a type that implements the dynamic Error trait,
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    // Box<dyn Error> means the function will return a type that implements the dynamic Error trait.
+
     // fs::read_to_string takes the filename, opens that file, and returns a Result<String> of the file’s contents:
     let contents = fs::read_to_string(config.filename)?;
     // Rather than panic! on an error, ? will return the error value from the current function for the caller to handle.
 
-    println!("With text:\n{}", contents);
-        Ok(())
+	for line in search(&config.query, &contents) {
+		println!("{}", line);
+	}
+
+	Ok(())
     // We’ve declared the run function’s success type as () in the signature, which means we need to wrap the unit type value in the Ok value. This Ok(()) syntax might look a bit strange at first, but using () like this is the idiomatic way to indicate that we’re calling run for its side effects only; it doesn’t return a value we need.
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+	let mut results = Vec::new();
+
+	for line in contents.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
+	}
+
+	results
+}
+
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str,
+) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
 
 #[cfg(test)]
@@ -39,13 +67,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- Rust/projects/minigrep/src/main.rs
+++ Rust/projects/minigrep/src/main.rs
@@ -14,8 +14,7 @@ fn main() {
         process::exit(1);
     }); // Destructures the variables returned by parse_config.
     
-    println!("Searching for {}", config.query);
-    println!("in file {}", config.filename);
+    println!("Searching for \"{}\" in file {}...", config.query, config.filename);
 
     if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
