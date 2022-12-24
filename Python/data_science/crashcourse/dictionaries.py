c836fa13e959a269becaadc267c8064e0aa18e7f
Paul Tuck <pau1tuck@outlook.com>
2022-12-24 13:45:48 +0700

    "2022-12-24_13:45+07"

---
+++ Python/data_science/crashcourse/dictionaries.py
@@ -0,0 +1,10 @@
+## Dictionaries
+
+empty_dict = {}
+grades = {"Xiaojun": 41, "Sarah": 78, "Wei": 87}
+wei_grade = grades["Wei"]
+num_students = len(grades)
+
+# Check if a key exists:
+jewel_has_grade = "Jewel" in grades     # False
+
--- Python/dictionaries.py
+++
@@ -1,5 +0,0 @@
-## Dictionaries
-
-empty_dict = {}
-grades = {"Xiaojun": 41, "Sarah": 78, "Wei": 87}
-wei_grade = grades["Wei"]
